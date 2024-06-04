7b461f133f74cdb9c36273adbf475611ed970ab5
bintangrf10 <rbintang206@gmail.com>
2024-06-04 19:41:21 +0700

    tugas 11

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,9 @@ Route::get('/products', function () {
 });
 
 Route::get('/movies',[MovieController::class, 'index']);
-
-Route::get('/genres',[GenreController::class, 'index']);
-
-Route::get('/reviews',[ReviewController::class, 'index']);
+Route::get('/movies/create', [MovieController::class, 'create']);
+Route::post('/movies', [MovieController::class, 'store']);
+Route::delete('/movies/{movie}', [MovieController::class, 'destroy']);
 
 
 
